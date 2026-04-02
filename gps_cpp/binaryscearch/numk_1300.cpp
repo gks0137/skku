6cad0f98f956932920a05df7f29c2ca67ed24dd3
gks0137 <stevenks8848@gmail.com>
2026-04-02 15:27:08 +0900

    오후 3:27 2026-04-02

--- gps_cpp/binaryscearch/numk_1300.cpp
+++ gps_cpp/binaryscearch/numk_1300.cpp
@@ -1,68 +1,62 @@
 #include <iostream>
 #include <deque>
+#include <vector>
+#include <cmath>
+
+int N, K;
+
+
+// int f_approx(int t) {
+//     if (t < N) {
+//         float temp = log(t) * t;
+//         return t + (int)floor(temp);
+//     } else {
+//         float temp = log((float)N*N/t) * t;
+//         return t + (int)floor(temp);
+//     }
+// }
+
+int f_abs(int t) {
+    int result = 0;
+    int high = std::min(N, t);
+    for (int i = 1; i <= high; i++) {
+        result += std::min(N, t/i);
+    }
+    return result;
+}
+
 
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
     std::cout.tie(NULL);
 
-    int n, k;
-    std::cin >> n >> k;
-
-    std::deque<std::deque<int>> increasing;
-
-    for (int i=0; i<n; i++) {
-        int num;
-        std::cin >> num;
-
-        if (increasing.empty()) {
-            increasing.push_back({num});
-            continue;
+    std::cin >> N >> K;
+    
+    int low = 1, high = K, mid, temp;
+    while (low < high) {
+        mid = low + (high - low) / 2;
+        temp = f_abs(mid);
+        if (temp < K) {
+            low = mid + 1;
+        } else {
+            high = mid;
         }
-        if (increasing.back().front() < num) {
-            increasing.push_back({num});
-            continue;
-        }
-
-
-        int low=0, high=increasing.size(), mid;
-        while (low <= high)
-        {
-            mid = (low + high) / 2;
-
-            if (increasing[mid][0]==num) {
-                break;
-            } else if (increasing[mid][0] < num) {
-                low = mid + 1;
-            } else {
-                high = mid-1;
-            }
-        }
-        if (increasing[mid][0] == num) continue;
-        if (increasing[mid][0] > num ) mid--;
-        if (mid<0) {
-            increasing.push_front({num});
-            continue;
-        }
-
-        int low=0, high=increasing.size(), mid2;
-        while (low <= high)
-        {
-            mid2 = (low + high) / 2;
-
-            if (increasing[mid][0]==num) {
-                break;
-            } else if (increasing[mid][0] < num) {
-                low = mid2 + 1;
-            } else {
-                high = mid2-1;
-            }
-        }
-        if (increasing[mid][mid2] > num) mid2--;
-
-        increasing[mid].emplace(increasing[mid].begin()+mid2, num);
-
     }
+    
+    std::cout << low << "\n";
+
+
+
+
+
+    // int t_temp = low;
+    // while (f_abs(t_temp) < K) {
+    //     t_temp++;
+    // }
+    // std::cout << t_temp << "\n";
+
+
 
 
 
