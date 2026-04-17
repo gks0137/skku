c4e2dc6200f8cf177c497dcf1e13bdf9347b8ad4
gks0137 <stevenks8848@gmail.com>
2026-04-17 19:06:55 +0900

    2026-04-17 19:06:57

--- gps/asdf/asdf.c
+++ gps/asdf/asdf.c
@@ -6,9 +6,16 @@ int main() {
 
     scanf("%d", &a);
 
-    printf("%d", a);
+    int dp[3] = {0, 1, 1};
+    for (int i = 3; i <= a; i++) {
+        dp[2] = dp[0] + dp[1];
+        dp[0] = dp[1];
+        dp[1] = dp[2];
+    }
+
+    printf("%d\n", dp[2]);
+
     
-    
-    
+
     return 0;
 }
