856fc6844239fabee2849f62bafce672046c110c
gks0137 <stevenks8848@gmail.com>
2026-04-24 10:58:17 +0900

    2026-04-24 10:58:19

--- gps/asdf/asdf.c
+++ gps/asdf/asdf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-#define add(x, y) x+y
+
 
 int main() {
 
-    short int a = -1;
-    unsigned int b = a;
+    
+    
 
-    printf("a: %d, b: %u\n", a, b);
 
     return 0;
 }
