4fa3e12ed31b0ce84f2eb59f00e40cd40ddf7f09
gks0137 <stevenks8848@gmail.com>
2026-04-28 09:03:07 +0900

    2026-04-28 09:03:09

--- gps/base.cpp
+++ gps/base.cpp
@@ -8,8 +8,22 @@ int main() {
     std::cout.tie(NULL);
 
 
-    std::cout << (3 & 2);
-
+    int M, N;
+    cin >> M >> N;
+    vector<bool> isPrime(N + 1, true);
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= N; ++i) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= N; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    for (int i = M; i <= N; ++i) {
+        if (isPrime[i]) {
+            cout << i << "\n";
+        }
+    }
     
 
     return 0;
