ce6897741da3ba5d83a95973d7bd43549350fa6d
gks0137 <stevenks8848@gmail.com>
2026-05-04 14:00:55 +0900

    2026-05-04 14:00:57

--- dasf004_44/Tetris_Windows/Tetris_Windows/hidden/tetris.c
+++ dasf004_44/Tetris_Windows/Tetris_Windows/hidden/tetris.c
@@ -5,19 +5,20 @@
 #include <time.h>
 
 // Screen dimensions
-const int SCREEN_WIDTH = 400;
-const int SCREEN_HEIGHT = 600;
+const int SCREEN_WIDTH = 420;
+const int SCREEN_HEIGHT = 500;
+const int FPS = 60;
+const int FRAME_DELAY = 1000 / FPS;
+const int BOARD_OFFSET_X = 120;
+const int BOARD_OFFSET_Y = 50;
 
 // Board dimensions
 const int BOARD_WIDTH = 10;
 const int BOARD_HEIGHT = 20;
 const int CELL_SIZE = 20;
 
-// Game board (0 = empty, 1-7 = filled with piece type)
-int board[BOARD_HEIGHT][BOARD_WIDTH] = {0};
-
 // Tetromino pieces (7 types, 4x4 grid)
-int tetromino[7][4][4] = {
+const int tetromino[7][4][4] = {
     // I piece (cyan)
     {{0,0,0,0},{1,1,1,1},{0,0,0,0},{0,0,0,0}},
     // O piece (yellow)
@@ -31,8 +32,16 @@ int tetromino[7][4][4] = {
     // L piece (orange)
     {{6,0,0,0},{6,6,6,0},{0,0,0,0},{0,0,0,0}},
     // J piece (blue)
-    {{0,7,0,0},{7,7,7,0},{0,0,0,0},{0,0,0,0}}
+    {{0,0,7,0},{7,7,7,0},{0,0,0,0},{0,0,0,0}}
 };
+
+// Bag system for random piece generation
+int bag[7] = {0, 1, 2, 3, 4, 5, 6};
+int bag_index = 0;
+
+// Game board (0 = empty, 1-7 = filled with piece type)
+int board[BOARD_HEIGHT][BOARD_WIDTH] = {0};
+
 
 // Color palette for pieces
 SDL_Color colors[8] = {
@@ -46,10 +55,12 @@ SDL_Color colors[8] = {
     {0, 0, 240, 255}      // 7: Blue (J)
 };
 
+
 typedef struct {
     int type;       // 0-6 for piece type
     int x, y;       // Top-left position on board
     int rotation;   // 0-3 for rotation state
+    int blocks[4][4]; // 4x4 grid for piece shape (after rotation)
 } Piece;
 
 typedef struct {
@@ -61,20 +72,32 @@ typedef struct {
     bool game_over;
 } GameState;
 
+// next piece queue (for preview)
+int next_piece_queue[5] = {0};
+
+// holding piece
+int hold_piece = -1;
+
 // Function prototypes
 bool canPlace(Piece p);
 void placePiece(Piece p);
 void spawnNewPiece(GameState* state);
 int clearLines(void);
-void rotatepiece(Piece* p);
+void rotatePieceC(Piece* p);
+void rotatePieceCC(Piece* p);
 void drawBoard(SDL_Renderer* renderer);
 void drawPiece(SDL_Renderer* renderer, Piece p);
+void hardDropPiece(GameState* state);
+void shuffleBag(void); 
+int getNextPiece(void);
+void swapHoldPiece(GameState* state);
+
 
 // Check if piece can be placed at current position
 bool canPlace(Piece p) {
     for (int row = 0; row < 4; row++) {
         for (int col = 0; col < 4; col++) {
-            if (tetromino[p.type][row][col] != 0) {
+            if (p.blocks[row][col] != 0) {
                 int bx = p.x + col;
                 int by = p.y + row;
                 
@@ -97,12 +120,12 @@ bool canPlace(Piece p) {
 void placePiece(Piece p) {
     for (int row = 0; row < 4; row++) {
         for (int col = 0; col < 4; col++) {
-            if (tetromino[p.type][row][col] != 0) {
+            if (p.blocks[row][col] != 0) {
                 int bx = p.x + col;
                 int by = p.y + row;
                 
                 if (by >= 0 && by < BOARD_HEIGHT && bx >= 0 && bx < BOARD_WIDTH) {
-                    board[by][bx] = tetromino[p.type][row][col];
+                    board[by][bx] = p.blocks[row][col];
                 }
             }
         }
@@ -111,10 +134,20 @@ void placePiece(Piece p) {
 
 // Spawn new piece at top
 void spawnNewPiece(GameState* state) {
-    state->current.type = rand() % 7;
+    state->current.type = next_piece_queue[0];
+    // Shift queue
+    for (int i = 0; i < 4; i++) {
+        next_piece_queue[i] = next_piece_queue[i + 1];
+    }
+    next_piece_queue[4] = getNextPiece();
     state->current.x = 3;
     state->current.y = 0;
     state->current.rotation = 0;
+    for (int row = 0; row < 4; row++) {
+        for (int col = 0; col < 4; col++) {
+            state->current.blocks[row][col] = tetromino[state->current.type][row][col];
+        }
+    }
     
     if (!canPlace(state->current)) {
         state->game_over = true;
@@ -154,14 +187,37 @@ int clearLines(void) {
 }
 
 // Rotate piece 90 degrees clockwise
-void rotatePiece(Piece* p) {
-    int old_type = p->type;
-    int old_rot = p->rotation;
-    p->rotation = (p->rotation + 1) % 4;
+void rotatePieceC(Piece* p) {
+    Piece temp = *p;
+    temp.rotation = (temp.rotation + 1) % 4;
+    for (int row = 0; row < 4; row++) {
+        for (int col = 0; col < 4; col++) {
+            temp.blocks[col][3 - row] = p->blocks[row][col];
+        }
+    }
     
     // Try rotation
-    if (!canPlace(*p)) {
-        p->rotation = old_rot; // Revert if collision
+    if (!canPlace(temp)) {
+        // Do nothing, keep original piece
+    } else {
+        *p = temp;
+    }
+}
+
+void rotatePieceCC(Piece* p) {
+    Piece temp = *p;
+    temp.rotation = (temp.rotation + 3) % 4; // Rotate left
+    for (int row = 0; row < 4; row++) {
+        for (int col = 0; col < 4; col++) {
+            temp.blocks[3 - col][row] = p->blocks[row][col];
+        }
+    }
+    
+    // Try rotation
+    if (!canPlace(temp)) {
+        // Do nothing, keep original piece
+    } else {
+        *p = temp;
     }
 }
 
@@ -170,8 +226,8 @@ void drawBoard(SDL_Renderer* renderer) {
     for (int row = 0; row < BOARD_HEIGHT; row++) {
         for (int col = 0; col < BOARD_WIDTH; col++) {
             SDL_Rect rect = {
-                col * CELL_SIZE,
-                row * CELL_SIZE,
+                col * CELL_SIZE + BOARD_OFFSET_X,
+                row * CELL_SIZE + BOARD_OFFSET_Y,
                 CELL_SIZE - 1,
                 CELL_SIZE - 1
             };
@@ -191,13 +247,13 @@ void drawBoard(SDL_Renderer* renderer) {
 void drawPiece(SDL_Renderer* renderer, Piece p) {
     for (int row = 0; row < 4; row++) {
         for (int col = 0; col < 4; col++) {
-            if (tetromino[p.type][row][col] != 0) {
+            if (p.blocks[row][col] != 0) {
                 int x = (p.x + col) * CELL_SIZE;
                 int y = (p.y + row) * CELL_SIZE;
                 
-                SDL_Rect rect = {x, y, CELL_SIZE - 1, CELL_SIZE - 1};
+                SDL_Rect rect = {x + BOARD_OFFSET_X, y + BOARD_OFFSET_Y, CELL_SIZE - 1, CELL_SIZE - 1};
                 
-                int color_idx = tetromino[p.type][row][col];
+                int color_idx = p.blocks[row][col];
                 SDL_SetRenderDrawColor(renderer, colors[color_idx].r, colors[color_idx].g, colors[color_idx].b, colors[color_idx].a);
                 SDL_RenderFillRect(renderer, &rect);
                 
@@ -207,6 +263,65 @@ void drawPiece(SDL_Renderer* renderer, Piece p) {
         }
     }
 }
+
+// Hard drop piece to the lowest valid position
+void hardDropPiece(GameState* state) {
+    while (true) {
+        state->current.y++;
+        if (!canPlace(state->current)) {
+            state->current.y--;
+            break;
+        }
+    }
+}
+
+// Shuffle the bag of pieces
+void shuffleBag(void) {
+    for (int i = 6; i > 0; i--) {
+        int j = rand() % (i + 1);
+        int temp = bag[i];
+        bag[i] = bag[j];
+        bag[j] = temp;
+    }
+}
+
+// Get next piece from bag
+int getNextPiece(void) {
+    if (bag_index >= 7) {
+        shuffleBag();
+        bag_index = 0;
+    }
+    return bag[bag_index++];
+}
+
+// Swap current piece with hold piece (or hold current piece if hold is empty)
+void swapHoldPiece(GameState* state) {
+    if (hold_piece == -1) {
+        hold_piece = state->current.type;
+        spawnNewPiece(state);
+    } else {
+        int temp = hold_piece;
+        hold_piece = state->current.type;
+        state->current.type = temp;
+        
+        // Update current piece blocks
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 4; col++) {
+                state->current.blocks[row][col] = tetromino[state->current.type][row][col];
+            }
+        }
+        
+        // Reset position and rotation
+        state->current.x = 3;
+        state->current.y = 0;
+        state->current.rotation = 0;
+        
+        if (!canPlace(state->current)) {
+            state->game_over = true;
+        }
+    }
+}
+
 
 int main(int argc, char* args[]) {
     srand(time(NULL));
@@ -237,10 +352,14 @@ int main(int argc, char* args[]) {
     GameState state = {0};
     state.fall_speed = 30; // Frames before piece falls
     state.fall_timer = 0;
+
+    for (int i = 0; i < 5; i++) {
+        next_piece_queue[i] = getNextPiece();
+    }
     
     spawnNewPiece(&state);
 
-    bool quit = false;
+    bool quit = false; 
     SDL_Event e;
     const Uint8* keystate;
 
@@ -258,7 +377,32 @@ int main(int argc, char* args[]) {
                         quit = true;
                         break;
                     case SDLK_SPACE:
-                        rotatePiece(&state.current);
+                        hardDropPiece(&state);
+                        state.fall_timer = state.fall_speed; // Force piece to lock immediately
+                        break;
+                    case SDLK_UP:
+                    case SDLK_x:
+                        rotatePieceC(&state.current);
+                        break;
+                    case SDLK_z:
+                        rotatePieceCC(&state.current);
+                        break;
+                    case SDLK_LEFT:
+                        state.current.x--;
+                        if (!canPlace(state.current)) {
+                            state.current.x++;
+                        }
+                        break;
+                    case SDLK_RIGHT:
+                        state.current.x++;
+                        if (!canPlace(state.current)) {
+                            state.current.x--;
+                        }
+                        break;
+                    case SDLK_c:
+                    case SDLK_LSHIFT:
+                    case SDLK_RSHIFT:
+                        swapHoldPiece(&state);
                         break;
                 }
             }
@@ -266,25 +410,9 @@ int main(int argc, char* args[]) {
 
         keystate = SDL_GetKeyboardState(NULL);
         
-        // Move left
-        if (keystate[SDL_SCANCODE_LEFT]) {
-            state.current.x--;
-            if (!canPlace(state.current)) {
-                state.current.x++;
-            }
-        }
-        
-        // Move right
-        if (keystate[SDL_SCANCODE_RIGHT]) {
-            state.current.x++;
-            if (!canPlace(state.current)) {
-                state.current.x--;
-            }
-        }
-        
         // Move down faster
         if (keystate[SDL_SCANCODE_DOWN]) {
-            state.fall_timer += 2;
+            state.fall_timer += 5; // Speed up fall
         }
 
         // Gravity
@@ -317,8 +445,38 @@ int main(int argc, char* args[]) {
         drawBoard(renderer);
         drawPiece(renderer, state.current);
 
+        // Draw next piece previews
+        for (int i = 0; i < 5; i++) {
+            Piece preview_piece;
+            preview_piece.type = next_piece_queue[i];
+            preview_piece.x = BOARD_WIDTH + 1; // Preview area to the right of board
+            preview_piece.y = i * 4; // Stack previews vertically
+            preview_piece.rotation = 0;
+            for (int row = 0; row < 4; row++) {
+                for (int col = 0; col < 4; col++) {
+                    preview_piece.blocks[row][col] = tetromino[preview_piece.type][row][col];
+                }
+            }
+            drawPiece(renderer, preview_piece);
+        }
+        
+        // Draw hold piece
+        if (hold_piece != -1) {
+            Piece hold_preview;
+            hold_preview.type = hold_piece;
+            hold_preview.x = -5; // Preview area to the left of board
+            hold_preview.y = 0; // Top of preview area
+            hold_preview.rotation = 0;
+            for (int row = 0; row < 4; row++) {
+                for (int col = 0; col < 4; col++) {
+                    hold_preview.blocks[row][col] = tetromino[hold_preview.type][row][col];
+                }
+            }
+            drawPiece(renderer, hold_preview);
+        }
+
         SDL_RenderPresent(renderer);
-        SDL_Delay(16);
+        SDL_Delay(FRAME_DELAY);
     }
 
     if (state.game_over) {
