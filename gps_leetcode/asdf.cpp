078b6a9f6146b2c17346975bc1f7c0e13848ed54
gks0137 <stevenks8848@gmail.com>
2026-05-19 13:51:01 +0900

    2026-05-19 13:51:03

--- gps_leetcode/asdf.cpp
+++ gps_leetcode/asdf.cpp
@@ -1,32 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        int uniqueCount = 0;
-        set<int> seen;
-        for (int num : nums) {
-            if (seen.find(num) == seen.end()) {
-                seen.insert(num);
-                nums[uniqueCount] = num;
-                uniqueCount++;
-            }
-        }
-        return uniqueCount;
-    }
-};  
-
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    Solution sol;
 
 
-    vector<int> nums = {1, 1, 2};
-    int result = sol.removeDuplicates(nums);
-    cout << result << "\n";
+    int a[] = {1, 2}, b = 5;
+    int *p1 = a;
+    int (*arrptr)[3] = (int (*)[3])&a;
+    int *arrofptr[3] = {&a[0], &a[1], &b};
+    int *(*arrofptrptr)[3] = &arrofptr;
+    int (**arrptrptr)[3] = (int (**)[3])&arrptr;
+
+    for (int i = 0; i < 3; i++) {
+        cout << *(*arrofptrptr)[i] << " ";
+    } cout << endl;
+    for (int i = 0; i < 3; i++) {
+        cout << (**arrptrptr)[i] << " ";
+    }
+
+
 
 
 
