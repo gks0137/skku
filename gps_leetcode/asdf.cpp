2b07cc43255eba93226f9bc22b55df72b2d7cc0a
gks0137 <stevenks8848@gmail.com>
2026-05-28 10:03:06 +0900

    2026-05-28 10:03:07

--- gps_leetcode/asdf.cpp
+++ gps_leetcode/asdf.cpp
@@ -4,27 +4,20 @@ using namespace std;
 
 class Solution {
 public:
-    void solveSudoku(vector<vector<char>>& board) {
-        function<bool(int, int)> backtrack = [&](int r, int c) {
-            if (r == 9) return true;
-            if (c == 9) return backtrack(r + 1, 0);
-            if (board[r][c] != '.') return backtrack(r, c + 1);
-            for (char num = '1'; num <= '9'; num++) {
-                bool valid = true;
-                for (int i = 0; i < 9; i++) {
-                    if (board[r][i] == num || board[i][c] == num || board[3 * (r / 3) + i / 3][3 * (c / 3) + i % 3] == num) {
-                        valid = false;
-                        break;
-                    }
-                }
-                if (!valid) continue;
-                board[r][c] = num;
-                if (backtrack(r, c + 1)) return true;
-                board[r][c] = '.';
+    string countAndSay(int n) {
+        if (n == 1) return "1";
+        string prev = countAndSay(n - 1);
+        string result;
+        int count = 1;
+        for (int i = 0; i < prev.length(); i++) {
+            if (i + 1 < prev.length() && prev[i] == prev[i + 1]) {
+                count++;
+            } else {
+                result += to_string(count) + prev[i];
+                count = 1;
             }
-            return false;
-        };
-        backtrack(0, 0);
+        }
+        return result;
     }
 };
 
@@ -35,7 +28,8 @@ int main() {
     cin.tie(0);
     Solution sol;
 
-
+    int n = 4;
+    cout << sol.countAndSay(n) << endl;
 
     return 0;
 }
--- gps_leetcode/asdf.py
+++ gps_leetcode/asdf.py
@@ -1,4 +1,24 @@
 import numpy as np
 from math import sqrt
-A = np.array([[2, 0, 5], [-1, 6, 2], [4, 4, -1], [5, 1, 0], [4, 1, 1]])
-print(np.linalg.matrix_rank(A))
+v1 = np.array([3, 0, 2, 0, 1, -1])
+v2 = np.array([3, -4, 5, 0, 2, 0])
+v3 = np.array([3, 0, 3, -4, 5, 0])
+
+def inner_product(v1, v2):
+    return sum(a * b for a, b in zip(v1, v2))
+
+u1 = v1
+u1_v2 = inner_product(u1, v2)
+u1_u1 = inner_product(u1, u1)
+u2 = v2 - (u1_v2 / u1_u1) * u1
+u1_v3 = inner_product(u1, v3)
+u2_v3 = inner_product(u2, v3)
+u2_u2 = inner_product(u2, u2)
+u3 = v3 - (u1_v3 / u1_u1) * u1 - (u2_v3 / u2_u2) * u2
+print("u1:", u1)
+print("u1_v2:", u1_v2)
+print("u1_u1:", u1_u1)
+print("u2:", u2)
+print("u2_v3:", u2_v3)
+print("u2_u2:", u2_u2)
+print("u3:", u3)
