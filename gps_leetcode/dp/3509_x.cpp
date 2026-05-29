142bec6c72a71f85c0d43cee57c1d3e559d84df4
gks0137 <stevenks8848@gmail.com>
2026-05-29 17:51:53 +0900

    2026-05-29 17:51:55

---
+++ gps_leetcode/dp/3509_x.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums, int k, int limit) {
+        
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    Solution sol;
+
+x
+    vector<int> nums = {0, 2, 3};
+    int k = -5, limit = 12;
+    cout << sol.maxProduct(nums, k, limit) << "\n";
+
+
+
+    return 0;
+}
