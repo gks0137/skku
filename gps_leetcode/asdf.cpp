cdfda05c02ee7b008041bdacd8840d6a22590c21
gks0137 <stevenks8848@gmail.com>
2026-06-09 09:59:30 +0900

    2026-06-09 09:59:31

--- gps_leetcode/asdf.cpp
+++ gps_leetcode/asdf.cpp
@@ -4,13 +4,44 @@ using namespace std;
 
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int max_sum = nums[0];
-        for (int i = 1; i < nums.size(); i++) {
-            nums[i] = max(nums[i], nums[i] + nums[i - 1]);
-            max_sum = max(max_sum, nums[i]);
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> result;
+        if (matrix.empty() || matrix[0].empty()) return result;
+
+        int top = 0, bottom = matrix.size() - 1;
+        int left = 0, right = matrix[0].size() - 1;
+
+        while (top <= bottom && left <= right) {
+            // right
+            for (int j = left; j <= right; j++) {
+                result.push_back(matrix[top][j]);
+            }
+            top++;
+
+            // down
+            for (int i = top; i <= bottom; i++) {
+                result.push_back(matrix[i][right]);
+            }
+            right--;
+
+            // left
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--) {
+                    result.push_back(matrix[bottom][j]);
+                }
+                bottom--;
+            }
+
+            // up
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.push_back(matrix[i][left]);
+                }
+                left++;
+            }
         }
-        return max_sum;
+
+        return result;
     }
 };
 
@@ -20,8 +51,11 @@ int main() {
     cin.tie(0);
     Solution sol;
 
-    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
-    cout << sol.maxSubArray(nums) << endl;
+    vector<vector<int>> matrix = {{1,2,3},{4,5,6},{7,8,9}};
+    vector<int> result = sol.spiralOrder(matrix);
+    for (int num : result) {
+        cout << num << " ";
+    }
     
     return 0;
 }
