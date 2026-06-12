2b3a8092c8be906d0e6b9542c46e5f87bca0d0ec
gks0137 <stevenks8848@gmail.com>
2026-06-12 21:30:31 +0900

    2026-06-12 21:30:33

--- gps_leetcode/asdf.cpp
+++ gps_leetcode/asdf.cpp
@@ -4,43 +4,39 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> merged;
-        bool inserted = false;
-
-        for (const auto& interval : intervals) {
-            if (!inserted && newInterval[1] < interval[0]) {
-                merged.push_back(newInterval);
-                inserted = true;
-            }
-            if (interval[1] < newInterval[0] || interval[0] > newInterval[1]) {
-                merged.push_back(interval);
-            } else {
-                newInterval[0] = min(newInterval[0], interval[0]);
-                newInterval[1] = max(newInterval[1], interval[1]);
-            }
+    int lengthOfLastWord(string s) {
+        int n = s.length();
+        int i = n - 1;
+        
+        // Skip trailing spaces
+        while (i >= 0 && s[i] == ' ') {
+            i--;
         }
-
-        if (!inserted) {
-            merged.push_back(newInterval);
+        
+        // Count the length of the last word
+        int length = 0;
+        while (i >= 0 && s[i] != ' ') {
+            length++;
+            i--;
         }
-
-        return merged;
+        
+        return length;
     }
 };
+
+
+
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     Solution sol;
 
-    vector<vector<int>> intervals = {{1,3},{6,9}};
-    vector<int> newInterval = {2,5};
-    vector<vector<int>> result = sol.insert(intervals, newInterval);
-    for (const auto& interval : result) {
-        cout << "[" << interval[0] << "," << interval[1] << "] ";
-    }
-    cout << endl;
+    string s = "Hello World";
+    cout << sol.lengthOfLastWord(s) << endl;
+
+
+
 
     return 0;
 }
