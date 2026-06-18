3e65cf0e1a2b3cc67a1ba1667ef920eab0960213
gks0137 <stevenks8848@gmail.com>
2026-06-18 09:18:09 +0900

    2026-06-18 09:18:10

--- gps_leetcode/asdfg.c
+++ gps_leetcode/asdfg.c
@@ -1,16 +1,43 @@
 #include <stdio.h>
-#include <string.h> 
 
 
 int main() {
     
-    char str[4] = "abc";
-    if (str[3] == '\0') {
-        puts("The string is null-terminated.");
-    } else {
-        puts("The string is not null-terminated.");
-    }
-    puts(str);
+    struct code {
+        char *country;
+        int code;
+    };
+
+    struct code code1 = {"USA", 1};
+
+    printf("%d %d %d\n", sizeof(code1), sizeof(code1.country), sizeof(code1.code));
     
+    union data {
+        int i;
+        double f;
+    };
+
+    union data d;
+    d.i = 10;
+    printf("%d %d\n", sizeof(d), sizeof(d.i));
+    d.f = 3.14;
+    printf("%d %d\n", sizeof(d), sizeof(d.f));
+
+    
+    typedef enum {
+        MONDAY = -5,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY = -4,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY
+    } Day;
+    Day today = WEDNESDAY;
+    printf("%d\n", today);
+
+    
+
+
     return 0;
 }
