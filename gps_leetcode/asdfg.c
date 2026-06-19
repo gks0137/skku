432b83f4ed1179a4db52409113819c533ade932d
gks0137 <stevenks8848@gmail.com>
2026-06-19 13:17:54 +0900

    2026-06-19 13:17:55

--- gps_leetcode/asdfg.c
+++ gps_leetcode/asdfg.c
@@ -1,41 +1,50 @@
 #include <stdio.h>
 
+// examples of fprintf, fscanf, fputc, fgetc, fwrite, fread,
 
 int main() {
     
-    struct code {
-        char *country;
-        int code;
-    };
-
-    struct code code1 = {"USA", 1};
-
-    printf("%d %d %d\n", sizeof(code1), sizeof(code1.country), sizeof(code1.code));
+    FILE *file = fopen("example.txt", "w");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+    // examples of fprintf, fputc, fwrite
+    fprintf(file, "1Hello, World!\n2Hello, World!\n3Hello, World%.1f!\n4Hello, World!\n", 42.0);
+    fputc('A', file);
+    putc('B', file);
+    fwrite("CDEFGHIJKL", sizeof(char), 10, file);
+    fclose(file);
     
-    union data {
-        int i;
-        double f;
-    };
-
-    union data d;
-    d.i = 10;
-    printf("%d %d\n", sizeof(d), sizeof(d.i));
-    d.f = 3.14;
-    printf("%d %d\n", sizeof(d), sizeof(d.f));
-
     
-    typedef enum {
-        MONDAY = -5,
+    FILE *file_read = fopen("example.txt", "r");
+    if (file_read == NULL) {
+        perror("Error opening file for reading");
+        return 1;
+    }
+    // examples of fscanf, fgetc, fread with ferror and feof
+    char buffer[5];
+    while (fscanf(file_read, "%c%c%c%c", &buffer[0], &buffer[1], &buffer[2], &buffer[3]) == 4) {
+        buffer[4] = '\0'; // Null-terminate the string
+        printf("Read string: %s\n", buffer);
+    }
+    printf("what happened to buffer: %s\n", buffer);
+    if (ferror(file_read)) {
+        perror("Error reading from file");
+    } else if (feof(file_read)) {
+        printf("End of file reached\n");
+    }
+
+
+    enum {
+        MONDAY = 1,
         TUESDAY,
-        WEDNESDAY,
-        THURSDAY = -4,
+        WEDNESDAY = -1,
+        THURSDAY,
         FRIDAY,
-        SATURDAY,
-        SUNDAY
-    } Day;
-    Day today = WEDNESDAY;
-    printf("%d\n", today);
+    } day;
 
+    day = MONDAY;
     
 
 
