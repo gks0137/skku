849be41780478f59ec02ed514a54411881b8e004
gks0137 <stevenks8848@gmail.com>
2026-07-06 17:04:34 +0900

    2026-07-06 17:04:35

--- gps_leetcode/asdf.cpp
+++ gps_leetcode/asdf.cpp
@@ -4,23 +4,22 @@ using namespace std;
 
 class Solution {
 public:
-    int countMajoritySubarrays(vector<int>& nums, int target) {
-        int n = nums.size();
-        int cur_state, ans = -1;
-        for (int i = 0; i < n; i++) {
-            cur_state = 0;
-            for (int j = i; j < n; j++) {
-                cur_state += nums[j] == target ? 1 : -1;
-                if (cur_state > 0) {
-                    ans++;
-                }
-            }
+    int findTheWinner(int n, int k) {
+        deque<int> dq;
+        for (int i = 1; i <= n; i++) {
+            dq.push_back(i);
         }
-        return ans;
+        n--;
+        while (n--) {
+            for (int i = 0; i < k - 1; i++) {
+                dq.push_back(dq.front());
+                dq.pop_front();
+            }
+            dq.pop_front();
+        }
+        return dq.front();
     }
 };
-
-
 
 
 int main() {
