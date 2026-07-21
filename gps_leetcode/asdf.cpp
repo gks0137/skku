e90f696e5036ce7e6366c7fabe78e761304acf20
gks0137 <stevenks8848@gmail.com>
2026-07-21 10:31:48 +0900

    2026-07-21 10:31:50

--- gps_leetcode/asdf.cpp
+++ gps_leetcode/asdf.cpp
@@ -4,46 +4,51 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size();
-        vector<int> parent(n + 1);
-        for (int i = 1; i <= n; ++i) {
-            parent[i] = i;
-        }
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int n = heights.size(), m = heights[0].size();
+        vector<vector<int>> ans;
+        vector<vector<bool>> pacific(n, vector<bool>(m, false));
+        vector<vector<bool>> atlantic(n, vector<bool>(m, false));
 
-        function<int(int)> find = [&](int x) {
-            if (parent[x] != x) {
-                parent[x] = find(parent[x]);
+        function<void(int, int, vector<vector<bool>>&)> dfs = [&](int i, int j, vector<vector<bool>>& ocean) {
+            if (ocean[i][j]) return;
+            ocean[i][j] = true;
+            vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+            for (auto& dir : directions) {
+                int x = i + dir.first, y = j + dir.second;
+                if (x >= 0 && x < n && y >= 0 && y < m && heights[x][y] >= heights[i][j]) {
+                    dfs(x, y, ocean);
+                }
             }
-            return parent[x];
         };
-        
-        vector<int> result;
 
-        for (const auto& edge : edges) {
-            int u = edge[0], v = edge[1];
-            int pu = find(u), pv = find(v);
-            if (pu == pv) {
-                result = edge;
-            }
-            parent[pu] = pv;
+        for (int i = 0; i < n; ++i) {
+            dfs(i, 0, pacific);
+            dfs(i, m - 1, atlantic);
         }
 
-        return result;
+        for (int j = 0; j < m; ++j) {
+            dfs(0, j, pacific);
+            dfs(n - 1, j, atlantic);
+        }
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    ans.push_back({i, j});
+                }
+            }
+        }
+
+        return ans;
+
     }
 };
-
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     Solution sol;
-
-    
-    vector<vector<int>> edges = {{1, 2}, {1, 3}, {2, 3}};
-    vector<int> result = sol.findRedundantConnection(edges);
-    cout << "[" << result[0] << ", " << result[1] << "]" << endl;
-
 
 
 
