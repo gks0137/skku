55d1fb372f7c9232481ca31a7df426fe93f9a7ec
gks0137 <stevenks8848@gmail.com>
2026-08-03 16:05:08 +0900

    2026-08-03 16:05:10

--- gps_leetcode/asdf.cpp
+++ gps_leetcode/asdf.cpp
@@ -1,29 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        int n = nums.size();
-        vector<int> cur;
+    int findKthLargest(vector<int>& nums, int k) {
+        // quick select algorithm
+        int left = 0, right = nums.size() - 1;
+        k = nums.size() - k; // convert to 0-based index for kth smallest
 
-        function<void(int)> backtrack = [&](int start) {
-            result.push_back(cur);
-            for (int i = start; i < n; ++i) {
-                cur.push_back(nums[i]);
-                backtrack(i + 1);
-                cur.pop_back();
+        while (left < right) {
+            int pivotIndex = partition(nums, left, right);
+            if (pivotIndex == k) {
+                return nums[pivotIndex];
+            } else if (pivotIndex < k) {
+                left = pivotIndex + 1;
+            } else {
+                right = pivotIndex - 1;
             }
-        };
+        }
 
-        backtrack(0);
-        return result;
+        return nums[left];
+    }
+
+private:
+    int partition(vector<int>& nums, int left, int right) {
+        int pivot = nums[right];
+        int i = left - 1;
+
+        for (int j = left; j < right; j++) {
+            if (nums[j] < pivot) {
+                i++;
+                swap(nums[i], nums[j]);
+            }
+        }
+        swap(nums[i + 1], nums[right]);
+        return i + 1;
     }
 };
-
-
 
 
 int main() {
@@ -31,10 +53,6 @@ int main() {
     cin.tie(0);
     Solution sol;
 
-    string beginWord = "hit", endWord = "cog";
-    vector<string> wordList = {"hot", "dot", "dog", "lot", "log", "cog"};
-
-    cout << sol.ladderLength(beginWord, endWord, wordList) << endl;
 
     return 0;
 }
