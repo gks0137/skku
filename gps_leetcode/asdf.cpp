2caf3b906d2146664db9bdbe7b92bcaf4f69739b
gks0137 <stevenks8848@gmail.com>
2026-08-14 12:22:26 +0900

    2026-08-14 12:22:27

--- gps_leetcode/asdf.cpp
+++ gps_leetcode/asdf.cpp
@@ -21,34 +21,49 @@ struct ListNode {
 
 class Solution {
 public:
-    string simplifyPath(string path) {
-        stack<string> st;
-        stringstream ss(path);
-        string token;
-        while (getline(ss, token, '/')) {
-            if (token == "" || token == ".") {
-                continue;
-            } else if (token == "..") {
-                if (!st.empty()) {
-                    st.pop();
-                }
-            } else {
-                st.push(token);
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        bool row0 = false, col0 = false;
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) {
+                col0 = true;
+                break;
             }
         }
-        if (st.empty()) {
-            return "/";
-        } else {
-            string res;
-            while (!st.empty()) {
-                res = "/" + st.top() + res;
-                st.pop();
+        for (int j = 0; j < m; j++) {
+            if (matrix[0][j] == 0) {
+                row0 = true;
+                break;
             }
-            return res;
+        }
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (row0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (col0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
         }
     }
 };
-
 
 
 
@@ -58,6 +73,16 @@ int main() {
     cin.tie(0);
     Solution sol;
 
+    vector<vector<int>> matrix = {{1,2,3,4},{5,0,7,8},{0,10,11,12},{13,14,15,0}};
+
+    sol.setZeroes(matrix);
+
+    for (const auto& row : matrix) {
+        for (const auto& val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
